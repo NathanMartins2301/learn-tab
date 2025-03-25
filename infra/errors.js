b4528b6c3a7843bb6e437d89da68d96ef80c109a
NathanMartins2301 <nathan.carneiro23011@gmail.com>
2025-03-25 10:44:37 -0300

    feat: add `MethodNotAllowed` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,11 +1,50 @@
 export class InternalServerErro extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Um erro interno não esperado aconteceu", {
       cause,
     });
     this.name = "Internal Server Error";
     this.action = "Entre em contato com o suporte.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceErro extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indísponível no momento", {
+      cause,
+    });
+    this.name = "Service Error";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Metodo não permitido para esse endpoint");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para esse endpoint.";
+    this.statusCode = 405;
   }
 
   toJSON() {
